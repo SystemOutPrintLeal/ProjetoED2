20fc6bc5b13185ae866fa2b5ef091c458e4bacff
SystemOutPrintLeal <victor_lealsk8@hotmail.com>
2021-03-08 13:02:04 -0300

    Add gitignore

--- include/Register.h
+++ include/Register.h
@@ -8,6 +8,7 @@
 
 typedef char * string;
 
+//Perfil empresa
 typedef struct Itens_empresa{
 
     string nome_item;
@@ -17,6 +18,7 @@ typedef struct Itens_empresa{
 
 }itens_empresa;
 
+//Perfil cliente
 typedef struct Itens_cliente{
 
     string nome_item;
@@ -26,27 +28,32 @@ typedef struct Itens_cliente{
 
 }itens_cliente;
 
+//Perfil empresa
 typedef struct Lista{
 
     itens_empresa* lista_empresa;
     struct Lista *prox;
 
-}lista;
+}lista_empresa;
 
+//Perfil cliente
 typedef struct Compras{
-    itens_cliente* compras_cliente;;
-    struct Compras* prox;
 
+    itens_cliente* compras_cliente;
+    struct Compras* prox;
 
 }compras_user;
 
+//Perfil Padrão
 typedef struct Info{
 
     //If tipo == Empresa
     string nome_empresa;
     //else
     string nome_usuario;
-
+    string sexo;
+    string numero_telefone;
+    string endereço;
     //Provavelmente um arquivo FILE.txt
 
 }info;
@@ -58,7 +65,24 @@ typedef struct Registro{
     string senha;
     info *InfoUser;
     struct Registro* prox;
+
 }registro;
+
+//Header funções para manipulação dos itens diretamente na memoria
+//Init
+itens_empresa* initItens_empresa();
+itens_cliente* initItens_cliente();
+lista_empresa* initLista_empresa();
+compras_user* initCompras_user();
+info* initInfo();
+registro* initRegistro();
+
+//Registra
+//Falta add parametros
+itens_empresa* registraItens_empresa();
+itens_cliente* registraItens_cliente();
+lista_empresa* registraLista_empresa();
+
 
 
 
